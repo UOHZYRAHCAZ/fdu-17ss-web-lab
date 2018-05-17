4131204ba3f115ce85267a54d76dfd317588c589
UOHZYRAHCAZ <1974647887zyc@gmail.com>
2018-05-17 08:28:51 +0800

    lab08

--- Lab08.php
+++ Lab08.php
@@ -5,9 +5,9 @@
 
 <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">    
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Lab08</title>
-      
+
     <link href='http://fonts.googleapis.com/css?family=Lobster' rel='stylesheet' type='text/css'>
     <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
 
@@ -18,26 +18,26 @@
 
 <body>
     <!-- include header here -->
-
+    <?php include 'header.inc.php'; ?>
     <!-- Page Content -->
     <main class="container">
         <div class="row">
-    
-        <!-- include left here -->          
-    
+
+        <!-- include left here -->
+    <?php include 'left.inc.php'; ?>
             <div class="col-md-10">
                 <div class="jumbotron" id="postJumbo">
                     <h1>Posts</h1>
                     <p>Read other travellers' posts ... or create your own.</p>
                     <p><a class="btn btn-warning btn-lg">Learn more &raquo;</a></p>
-                </div>        
-      
+                </div>
+
                 <!-- start post summaries -->
                 <div class="postlist">
 
                     <!-- replace each of these rows with a function call -->
-                    <?php             
-                    outputPostRow(1); 
+                    <?php
+                    outputPostRow(1);
                     outputPostRow(2);
                     outputPostRow(3);
                     ?>
@@ -46,7 +46,7 @@
             </div>  <!-- end col-mid-10 -->
         </div>  <!-- end row -->
     </main>
-    
+
 
         <script src="https://code.jquery.com/jquery-2.2.4.min.js" integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44=" crossorigin="anonymous"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
--- functions.inc.php
+++ functions.inc.php
@@ -10,6 +10,54 @@ function generateLink($url, $label, $class) {
 
 function outputPostRow($number)  {
     include("travel-data.inc.php");
+    $postId="postId".$number;
+    $postId=$$postId;
+    $userId="userId".$number;
+    $userId=$$userId;
+    $userName="userName".$number;
+    $userName=$$userName;
+    $date="date".$number;
+    $date=$$date;
+    $thumb="thumb".$number;
+    $thumb=$$thumb;
+    $title="title".$number;
+    $title=$$title;
+    $excerpt="excerpt".$number;
+    $excerpt=$$excerpt;
+    $reviewsNum="reviewsNum".$number;
+    $reviewsNum=$$reviewsNum;
+    $reviewsRating="reviewsRating".$number;
+    $reviewsRating=$$reviewsRating;
+    echo '
+    <div class="row">
+      <div class="col-md-4"><a href=';
+    echo "post.php?id=".$postId.' class="">
+      <img src=';
+      echo "images/".$thumb.' alt=';
+      echo $title.' class="img-responsive"/>
+      </a>
+      </div>
+      <div class="col-md-8">
+       <h2>';
+     echo $title.'</h2>
+       <div class="details">Posted by <a href=';echo "user.php?id=".$userId.' class="">';echo $userName.'</a>
+       <span class="pull-right">';echo $date.'</span>
+       <p class="ratings">';
+       for ($i=0;$i<$reviewsRating;$i++){
+         echo '<img src="images/star-gold.svg" width="16" />';
+       }
+       if($reviewsRating<5)
+       for($j=$reviewsRating;$j<5;$j++){echo '<img src="images/star-white.svg" width="16" />';}
+
+
+       echo $reviewsNum.' Reviews</p>
+       </div>
+       <p class="excerpt">';echo $excerpt.'</p>
+       <p><a href=';echo "post.php?id=".$postId.' class="btn btn-primary btn-sm">Read more</a>
+       </p>
+       </div>
+       </div>
+       <hr/>;';
 }
 
 /*
@@ -18,18 +66,18 @@ function outputPostRow($number)  {
 */
 function constructRating($rating) {
     $imgTags = "";
-    
+
     // first output the gold stars
     for ($i=0; $i < $rating; $i++) {
         $imgTags .= '<img src="images/star-gold.svg" width="16" />';
     }
-    
+
     // then fill remainder with white stars
     for ($i=$rating; $i < 5; $i++) {
         $imgTags .= '<img src="images/star-white.svg" width="16" />';
-    }    
-    
-    return $imgTags;    
+    }
+
+    return $imgTags;
 }
 
 ?>
